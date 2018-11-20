f95fe78ea23bef948a61f7a6ae278bca050563d0
falos-iscteiul <falos@iscte.pt>
2018-11-20 20:42:36 +0000

    Melhoramento da interface

--- ES1-69/src/InterGraf.java
+++ ES1-69/src/InterGraf.java
@@ -1,19 +1,39 @@
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.GridLayout;
 import javax.swing.JButton;
-import java.awt.GridBagConstraints;
+import javax.swing.JTree;
+import javax.swing.JMenu;
+import javax.swing.JOptionPane;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JSlider;
+import javax.swing.JScrollPane;
+import java.awt.List;
+import java.awt.Scrollbar;
+import java.awt.ScrollPane;
+import javax.swing.SwingConstants;
+import java.awt.Panel;
 import javax.swing.JTextField;
-import java.awt.Insets;
-import javax.swing.JCheckBox;
-import javax.swing.JTextPane;
-import javax.swing.JInternalFrame;
+import javax.swing.DropMode;
+import javax.swing.UIManager;
+import java.awt.SystemColor;
+import java.awt.Font;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
 
-public class InterGraf {	
-	
-	private JFrame frame;
-	private JTextField textField;
+public class InterGraf extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtSearch;
+
 	/**
 	 * Launch the application.
 	 */
@@ -21,8 +41,8 @@ public class InterGraf {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					InterGraf window = new InterGraf();
-					window.frame.setVisible(true);
+					InterGraf frame = new InterGraf();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -31,71 +51,61 @@ public class InterGraf {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public InterGraf() {
-		initialize();
+		setBackground(UIManager.getColor("EditorPane.inactiveForeground"));
+		setForeground(Color.WHITE);
+		setTitle("ISCTE_APP");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 476, 342);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		Scrollbar scrollbar = new Scrollbar();
+		scrollbar.setBackground(SystemColor.inactiveCaptionBorder);
+		scrollbar.setBounds(390, 83, 21, 186);
+		contentPane.add(scrollbar);
+		
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setBackground(SystemColor.inactiveCaptionBorder);
+		scrollPane.setBounds(158, 83, 226, 186);
+		contentPane.add(scrollPane);
+		
+		txtSearch = new JTextField();
+		txtSearch.setText("\r\n");
+		txtSearch.setBackground(SystemColor.inactiveCaptionBorder);
+		txtSearch.setToolTipText("Search");
+		txtSearch.setBounds(158, 42, 226, 22);
+		contentPane.add(txtSearch);
+		txtSearch.setColumns(10);
+		
+		JButton btnNewButton_3 = new JButton("Post");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String inputValue = JOptionPane.showInputDialog("User:");
+				String inputValue1 = JOptionPane.showInputDialog("Password:");
+				String inputValue2 = JOptionPane.showInputDialog("To:");
+				String inputValue3 = JOptionPane.showInputDialog("Subject");
+				String inputValue4 = JOptionPane.showInputDialog("Message");
+			}
+		});
+		btnNewButton_3.setBounds(46, 244, 97, 25);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton = new JButton("Facebook");
+		btnNewButton.setBounds(46, 83, 97, 25);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Twitter");
+		btnNewButton_1.setBounds(46, 121, 97, 25);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Email");
+		btnNewButton_2.setBounds(46, 159, 97, 25);
+		contentPane.add(btnNewButton_2);
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 580, 360);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 469, 5, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
-		
-		JButton btnSearch = new JButton("Search");
-		GridBagConstraints gbc_btnSearch = new GridBagConstraints();
-		gbc_btnSearch.insets = new Insets(0, 0, 5, 5);
-		gbc_btnSearch.gridx = 0;
-		gbc_btnSearch.gridy = 0;
-		frame.getContentPane().add(btnSearch, gbc_btnSearch);
-		
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 0;
-		frame.getContentPane().add(textField, gbc_textField);
-		textField.setColumns(10);
-		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Facebook");
-		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
-		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 5, 5);
-		gbc_chckbxNewCheckBox.gridx = 0;
-		gbc_chckbxNewCheckBox.gridy = 1;
-		frame.getContentPane().add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
-		
-		JCheckBox chckbxTwitter = new JCheckBox("Twitter");
-		GridBagConstraints gbc_chckbxTwitter = new GridBagConstraints();
-		gbc_chckbxTwitter.insets = new Insets(0, 0, 5, 5);
-		gbc_chckbxTwitter.gridx = 0;
-		gbc_chckbxTwitter.gridy = 2;
-		frame.getContentPane().add(chckbxTwitter, gbc_chckbxTwitter);
-		
-		JCheckBox chckbxEmail = new JCheckBox("Email");
-		GridBagConstraints gbc_chckbxEmail = new GridBagConstraints();
-		gbc_chckbxEmail.insets = new Insets(0, 0, 5, 5);
-		gbc_chckbxEmail.gridx = 0;
-		gbc_chckbxEmail.gridy = 3;
-		frame.getContentPane().add(chckbxEmail, gbc_chckbxEmail);
-		
-		JTextPane textPane = new JTextPane();
-		GridBagConstraints gbc_textPane = new GridBagConstraints();
-		gbc_textPane.insets = new Insets(0, 0, 0, 5);
-		gbc_textPane.gridheight = 7;
-		gbc_textPane.fill = GridBagConstraints.BOTH;
-		gbc_textPane.gridx = 1;
-		gbc_textPane.gridy = 1;
-		frame.getContentPane().add(textPane, gbc_textPane);
-	}
-
 }
