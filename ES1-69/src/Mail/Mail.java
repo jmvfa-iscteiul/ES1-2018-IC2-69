f8e0614f7cecc16d4377118f9dbe7856be702057
jmvfa-iscteiul <jmvfa@iscte-iul.pt>
2018-11-21 04:16:02 +0000

    2nd Mail commit - corrigir erros

--- ES1-69/src/Mail/Mail.java
+++ ES1-69/src/Mail/Mail.java
@@ -1,98 +1,110 @@
 package Mail;
 
-import javax.mail.PasswordAuthentication;
-
-import java.awt.EventQueue;
-import java.util.*;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.Properties;
+import java.util.Scanner;
 
+import javax.mail.Address;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.NoSuchProviderException;
+import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
 
+public class Mail {
 
-public class Mail{
-	
-	public static void procurar() throws NoSuchProviderException  {
-		
-		String host = "pop.outlook.com";
-		Scanner s = new Scanner(System.in);
-		System.out.print("User: ");
-		final String username = s.nextLine();
-		System.out.println("aqui4");
-		System.out.print("Password");
-		final String password = s.nextLine();
+   public static void fetch(String pop3Host, String storeType, String user,String password, String procura) {
+      try {
+         // create properties field
+         Properties properties = new Properties();
+         properties.put("mail.store.protocol", "pop3");
+         properties.put("mail.pop3.host", pop3Host);
+         properties.put("mail.pop3.port", "993");
+         properties.put("mail.pop3.starttls.enable", "true");
+         Session emailSession = Session.getDefaultInstance(properties);
+         // emailSession.setDebug(true);
 
-		System.out.print("O que procura? ");
-		String procura = s.nextLine();
-		System.out.println(username);
+         // create the POP3 store object and connect with the pop server
+         Store store = emailSession.getStore("pop3s");
+
+         store.connect(pop3Host, user, password);
+
+         // create the folder object and open it
+         Folder emailFolder = store.getFolder("INBOX");
+         emailFolder.open(Folder.READ_ONLY);
+
+         BufferedReader reader = new BufferedReader(new InputStreamReader(
+	      System.in));
+
+         // retrieve the messages from the folder in an array and print it
+         Message[] messages = emailFolder.getMessages();
+         System.out.println("messages.length---" + messages.length);
+
+         for (int i = 0; i < 100; i++) {
+            Message message = messages[i];
+            if(message.getContent().toString().contains(procura)) {
+               	System.out.println("________________");
+				System.out.println("Email nº" + (i+1));
+				System.out.println("Subject: " + message.getSubject());
+				System.out.println("From: " + message.getFrom());
+				System.out.println("Text: " + message.getContent().toString());
+            	
+            	//String line = reader.readLine();
+            	//if ("YES".equals(line)) {
+            	//	message.writeTo(System.out);
+            	//} else if ("QUIT".equals(line)) {
+            	//	break;
+            	//}
+            }
+         }
+
+       
+         emailFolder.close(false);
+         store.close();
+
+      } catch (NoSuchProviderException e) {
+         e.printStackTrace();
+      } catch (MessagingException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   public static void main(String[] args) {
+
+      String host = "pop.outlook.com";// change accordingly
+      String mailStoreType = "pop3";
+      
+      Scanner scanner = new Scanner(System.in);
 		
-			System.out.println("aqui1");
-			Properties prop = new Properties();
-			
-			prop.put("mail.pop3.host", host);
-			prop.put("mail.pop3.port", "993");
-			prop.put("mail.pop3.starttls.enable", "true");
-			
-			//Session session = Session.getDefaultInstance(prop);
-			Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(username, password);
-					
-				}
-			});
-			Store store = session.getStore("pop3s");
-			
-			try {
-			store.connect(host, username, password);
-			
-			Folder emailFolder = store.getFolder("INBOX");
-			emailFolder.open(Folder.READ_ONLY);
-			
-			Message[] messages = emailFolder.getMessages();
-			System.out.println("messages.length---" + messages.length);
-			
-			int n = messages.length;
-			for(int i = 0; i < 100; i++) {
-				Message message = messages[i];
-				//boolean existeProcura = message.getContent().toString().contains(procura);
-				if(message.getContent().toString().contains(procura)) {
-					System.out.println("______");
-					System.out.println("Email nº" + (i+1));
-					System.out.println("Subject: " + message.getSubject());
-					System.out.println("From: " + message.getFrom());
-					System.out.println("Text: " + message.getContent().toString());
-				}
-			}
-			
-			emailFolder.close(false);
-			store.close();
-			
-			
-			}catch(MessagingException e) {
-				e.printStackTrace();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-			s.close();
-	
-	}
-	
-	public static void main(String args[])  {
-		EventQueue.invokeLater(new Runnable() {
-		public void run() {
-		try {
-			procurar();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		}
-		});	
-	}
-	
-	
+      System.out.print("User: ");
+      final String username = scanner.nextLine();
+		
+      System.out.print("Password");
+      final String password = scanner.nextLine();
+
+      System.out.print("O que procura? ");
+		String procura = scanner.nextLine();
+		
+		scanner.close();
+
+      //Call method fetch
+      fetch(host, mailStoreType, username, password, procura);
+
+   }
+
+
 }
